a96f61e35e869a5f08c74fc3585105584da5366a
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-12-02 14:25:57 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { setName, setEmail } from "../redux/userSlice";
+
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { name, email } = useSelector(state => state.user);
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="page">
+      <h1>User Information</h1>
+
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={e => dispatch(setName(e.target.value))}
+        /> <br/> <br/>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={e => dispatch(setEmail(e.target.value))}
+        />
+      </div>
+
+      <div className="output">
+        <p>Current values in store:</p>
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
     </div>
   )
 }
